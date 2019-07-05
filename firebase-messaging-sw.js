1c0c68ab4f4f5366314035cf4e93ee7a349ce728
chlekthf3915 <52516064+chlekthf3915@users.noreply.github.com>
2019-07-05 16:25:44 +0900

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/6.2.4/firebase/6.2.4/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/6.2.4/firebase/6.2.4/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/6.2.4/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/6.2.4/firebase-messaging.js');
 importScripts('scripts/init.js');
 
 firebase.messaging();
