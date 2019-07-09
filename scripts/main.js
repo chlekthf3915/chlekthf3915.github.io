c46ca25c582773b1d17d6a55934dd52fd064ec48
chlekthf3915 <52516064+chlekthf3915@users.noreply.github.com>
2019-07-09 15:57:49 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -81,7 +81,7 @@ function loadMessages() {
   //.onSnapshot는 쿼리와 일치하는 문서가 변경되면 콜백함수가 트리거됨
   query.onSnapshot(function(snapshot){
 	  var obj = [];
-	  snapshot.docChanges().orderBy('timestamp', 'asc').forEach(function(change){
+	  snapshot.docChanges.orderBy('timestamp', 'asc').forEach(function(change){
 		  if(change.type == 'removed')
 			  deleteMessage(change.doc.id);
 		  else{
